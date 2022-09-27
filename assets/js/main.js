a4e5543c68bca2d6bfdb122c1ff58c6c82a4786a
mikecebul <mikecebul@gmail.com>
2022-09-27 17:35:01 -0400

    animate speed instant

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function ($) {
-  $(".level-bar-inner").css("width", "level");
+  $(".level-bar-inner").css("width", "0");
 
   $(window).on("load", function () {
     $(".level-bar-inner").each(function () {
